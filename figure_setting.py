3212f849a3bd87d013a22a365b9aad3581c55a65
WataruTakahagi <watarut@outlook.com>
2019-11-06 16:04:32 +0900

    Add figure setting

---
+++ figure_setting.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+plt.rcParams['font.family'] = 'arial'
+plt.rcParams['font.size'] = 18
+plt.rcParams['xtick.direction'] = 'in'
+plt.rcParams['ytick.direction'] = 'in'
+plt.rcParams['xtick.major.width'] = 1.0
+plt.rcParams['ytick.major.width'] = 1.0
+plt.rcParams['lines.linewidth'] = 0.8
+
+plt.grid(which='major',color='lightgray',linestyle='--')
+plt.grid(which='minor',color='lightgray',linestyle='--')
