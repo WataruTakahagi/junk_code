ab4bdbc61f53302376367364057598b88201d248
WataruTakahagi <watarut@outlook.com>
2020-04-28 07:53:07 +0900

    Update

--- figure_setting.py
+++ figure_setting.py
@@ -12,4 +12,5 @@ plt.grid(which='major',color='lightgray',linestyle='--')
 plt.grid(which='minor',color='lightgray',linestyle='--')
 
 npg = mpl.colors.ListedColormap(['#E64B3599','#4DBBD599','#00A08799','#3C548899','#F39B7F99','#8491B499','#91D1C299','#DC000099','#7E614899','#B09C8599'])
-npg_clist = ['#E64B3599','#4DBBD599','#00A08799','#3C548899','#F39B7F99','#8491B499','#91D1C299','#DC000099','#7E614899','#B09C8599']
+npg_clist = ['#E64B35FF','#4DBBD5FF','#00A087FF','#3C5488FF','#F39B7FFF','#8491B4FF','#91D1C2FF','#DC0000FF','#7E6148FF','#B09C85FF']
+npg_clist06 = ['#E64B3599','#4DBBD599','#00A08799','#3C548899','#F39B7F99','#8491B499','#91D1C299','#DC000099','#7E614899','#B09C8599']
