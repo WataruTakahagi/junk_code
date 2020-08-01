fe6ecaa2b51e693b977bb94a2c1a7451af47b24f
WataruTakahagi <watarut@outlook.com>
2020-08-01 18:04:52 +0900

    Update

--- publishable_setting_tips.py
+++ publishable_setting_tips.py
@@ -1,4 +1,29 @@
-#軸消す
+import numpy as np
+import pandas as pd
+import math
+from pylab import *
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import matplotlib.ticker as ticker
+from matplotlib.ticker import ScalarFormatter
+import matplotlib
+matplotlib.font_manager._rebuild()
+
+npg_clist = ['#E64B35FF','#4DBBD5FF','#00A087FF','#3C5488FF','#F39B7FFF','#8491B4FF','#91D1C2FF','#DC0000FF','#7E6148FF','#B09C85FF'] #NPG Journal Color Palettes (https://nanx.me/ggsci/index.html)
+
+plt.rcParams['font.family']= 'sans-serif'
+plt.rcParams['font.sans-serif'] = ['arial']
+plt.rcParams['font.size'] = 15
+plt.rcParams['xtick.direction'] = 'out'
+plt.rcParams['ytick.direction'] = 'out'
+plt.rcParams['xtick.major.width'] = 0.5
+plt.rcParams['ytick.major.width'] = 0.5
+plt.rcParams['axes.linewidth'] = 0.5
+
+plt.grid(which='major',color='lightgray',linestyle='--')#Major Grid
+plt.grid(which='minor',color='lightgray',linestyle='--')#Minor Grid
+
+#Delete Axis
 ax.tick_params(labelbottom=False,
                labelleft=False,
                labelright=False,
